b9ac2f37363ee6d0ba575bca813aed1667140791
ulyssepdc <ulyssepdc@gmail.com>
2025-09-22 11:29:04 +0000

    message explicatif

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ from math import sqrt
 
 
 def isprime(p):
+    if p in [0,1,2]:
+        return False
+    for i in range(2,p,1):
+        if p % i == 0:
+            return False
+    return True
+            
+    
+    
+
 
     # votre code ici
 
